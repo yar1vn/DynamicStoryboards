22a798e074c160346a02b024520e9afa3f0a39fd
Yariv Nissim <ynissim@truecar.com>
2016-02-22 18:34:53 -0800

    Initial Commit

--- DynamicStoryboards/ViewController.swift
+++ DynamicStoryboards/ViewController.swift
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public class MyViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override public var nibName: String? {
+        return "MyViewController"
+    }
+    
+    public override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func show(sender: UIButton) {
+        performSegueWithIdentifier("Show", sender: sender)
     }
-
-
 }
 
